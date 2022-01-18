0cac26831ed772ce1d1fae6215793dcc1a6ea570
LeatherbacksUMD <97926586+LeatherbacksUMD@users.noreply.github.com>
2022-01-17 20:43:40 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,67 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+
+TOKEN = os.environ.get('TOKEN')
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected')
+    global guild, rules, hubris, kerfuffle, open_combat, plastic_ants
+    guild = client.get_guild(928046831013879889)
+    rules = client.get_channel(931952764572020736)
+    hubris = client.get_emoji(932764756903874562)
+    kerfuffle = client.get_emoji(932752842916233257)
+    open_combat = guild.get_role(932767230318477343)
+    plastic_ants = guild.get_role(932766907986214922)
+
+    embed = discord.Embed(color=0xe03a3e)
+    embed.add_field(name='Rules', value='1. Treat everyone with respect. '
+                    'Absolutely no harassment, witch hunting, sexism, '
+                    'racism, or hate speech will be tolerated.\n2. No NSFW'
+                    ' or obscene content. This includes text, images, or '
+                    'links featuring nudity, sex, hard violence, or other '
+                    'graphically disturbing content.\n3. If you see '
+                    'something against the rules or something that makes '
+                    'you feel unsafe, let the officers know. We want this '
+                    'server to be a welcoming space!', inline=False)
+    embed.add_field(name='Recommendations', value='1. It would be helpful '
+                    'to everyone if you could make your nickname for this '
+                    'server your real name (or what you want to go by)!\n'
+                    '2. You\'re welcome to participate in whatever '
+                    'category of bot you see fit! To indicate this, please'
+                    f' react with {hubris} if you want to get into open '
+                    f'combat (larger bots) and with {kerfuffle} if you '
+                    'want to get into plastic ants (1lb). If you don\'t '
+                    'want to receive notifications from the part(s) of the'
+                    ' club you\'re not involved in, please change your '
+                    'notification settings for that channel ONLY and not '
+                    'the entire server.', inline=False)
+    global rule
+    rule = await rules.send(embed=embed)
+    await rule.add_reaction(kerfuffle)
+    await rule.add_reaction(hubris)
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+    if user != client.user and reaction.message == rule:
+        if reaction.emoji == hubris:
+            await user.add_roles(open_combat)
+        elif reaction.emoji == kerfuffle:
+            await user.add_roles(plastic_ants)
+
+
+@client.event
+async def on_reaction_remove(reaction, user):
+    if reaction.message == rule:
+        if reaction.emoji == hubris:
+            await user.remove_roles(open_combat)
+        elif reaction.emoji == kerfuffle:
+            await user.remove_roles(plastic_ants)
+
+client.run(TOKEN)
